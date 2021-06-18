493b3624a6ab1b5bff00886d1a25f2d2b8519a83
Ethan Edmond <eedmond@uccs.edu>
2021-06-18 11:14:44 -0600

    Finished PrivateRoute, might run into bugs later

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,13 @@
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 function PrivateRoute(props){
   return (
-    <Route {...props}/>
+    localStorage.getItem('token') ?
+      <Route {...props}/> :
+      <Redirect to='/'/>
   )
 };
 
