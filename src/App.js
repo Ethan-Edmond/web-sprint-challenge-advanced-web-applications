926dc00c8f44e16573bd17819bd0a0019b019d22
Ethan Edmond <eedmond@uccs.edu>
2021-06-18 11:42:54 -0600

    Finished logout button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import axiosWithAuth from './helpers/axiosWithAuth';
 import Login from "./components/Login";
 import PrivateRoute from './components/PrivateRoute';
 import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
+  const logout = (e) => {
+    e.preventDefault();
+    window.localStorage.removeItem('token');
+    window.location.href = '/';
+  }
+    
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <button data-testid="logoutButton" onClick={logout}>logout</button>
         </header> 
 
         <Route exact path="/" component={Login} />
